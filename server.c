bc01d8e9d4c7d0b17662ab26b0a64deffb0a44b2
ahnahn <80297360+ahnahn@users.noreply.github.com>
2025-04-28 02:17:53 +0900

    Update server.c

--- server.c
+++ server.c
@@ -7,7 +7,7 @@
 #include <netinet/in.h>
 #include "chess.h"
 
-#define PORT 5000       /* Server port */
+#define PORT 5000
 #define BUF_SIZE 256
 
 int client_sock[2];     /* client sockets for WHITE=0, BLACK=1 */
@@ -21,28 +21,38 @@ typedef struct {
     int color;         /* 0 for White, 1 for Black */
 } ThreadData;
 
+/* Send a message to a client */
+void send_msg(int sock, const char *msg) {
+    send(sock, msg, strlen(msg), 0);
+}
+
 /* Broadcast the current board to both clients */
 void broadcast_board() {
     char buf[BUF_SIZE];
-    /* Generate board ASCII into buffer */
-    /* For simplicity, we print to stdout on server and send same */
-    printf("Current board:\n");
-    print_board(&game);
-    /* Send board as text to clients */
-    for(int i=0; i<2; i++) {
-        FILE *fp = fdopen(client_sock[i], "w");
-        if(!fp) continue;
-        fprintf(fp, "\n");
-        fprintf(fp, "  a b c d e f g h\n");
-        for(int r = 0; r < BOARD_SIZE; r++) {
-            fprintf(fp, "%d ", BOARD_SIZE - r);
-            for(int c = 0; c < BOARD_SIZE; c++) {
-                fprintf(fp, "%c ", game.board[r][c]);
-            }
-            fprintf(fp, "%d\n", BOARD_SIZE - r);
+    snprintf(buf, sizeof(buf), "\n  a b c d e f g h\n");
+    for (int i = 0; i < 2; i++) {
+        send(client_sock[i], buf, strlen(buf), 0);
+    }
+
+    for (int r = 0; r < BOARD_SIZE; r++) {
+        snprintf(buf, sizeof(buf), "%d ", BOARD_SIZE - r);
+        for (int c = 0; c < BOARD_SIZE; c++) {
+            char piece[4];
+            snprintf(piece, sizeof(piece), "%c ", game.board[r][c]);
+            strncat(buf, piece, sizeof(buf) - strlen(buf) - 1);
         }
-        fprintf(fp, "  a b c d e f g h\n");
-        fflush(fp);
+        char endline[16];
+        snprintf(endline, sizeof(endline), "%d\n", BOARD_SIZE - r);
+        strncat(buf, endline, sizeof(buf) - strlen(buf) - 1);
+
+        for (int i = 0; i < 2; i++) {
+            send(client_sock[i], buf, strlen(buf), 0);
+        }
+    }
+
+    snprintf(buf, sizeof(buf), "  a b c d e f g h\n");
+    for (int i = 0; i < 2; i++) {
+        send(client_sock[i], buf, strlen(buf), 0);
     }
 }
 
@@ -54,16 +64,14 @@ void *client_thread(void *arg) {
     char buf[BUF_SIZE];
 
     /* Assign color */
-    FILE *client_fp = fdopen(client_sock[me], "r+");
-    if(me == WHITE) {
-        fprintf(client_fp, "You are WHITE. Waiting for Black...\n");
+    if (me == WHITE) {
+        send_msg(client_sock[me], "You are WHITE. Waiting for Black...\n");
     } else {
-        fprintf(client_fp, "You are BLACK. Starting game...\n");
+        send_msg(client_sock[me], "You are BLACK. Starting game...\n");
     }
-    fflush(client_fp);
 
     /* Wait for both clients ready */
-    if(me == BLACK) {
+    if (me == BLACK) {
         /* When Black connects, broadcast initial board */
         broadcast_board();
     }
@@ -73,25 +81,24 @@ void *client_thread(void *arg) {
         pthread_mutex_lock(&game_mutex);
         /* Wait for our turn */
         while (game.turn != me) {
-            /* Check for end of game */
             if (game.turn == -1) {
                 pthread_mutex_unlock(&game_mutex);
                 goto game_end;
             }
             pthread_cond_wait(&turn_cond, &game_mutex);
         }
-        /* It's our turn now */
+
         /* Check for checkmate or stalemate */
         if (is_in_check(&game, me)) {
             if (!has_valid_moves(&game, me)) {
-                /* Checkmate */
                 broadcast_board();
-                if(me == WHITE) {
-                    fprintf(client_fp, "Checkmate! BLACK wins.\n");
+                if (me == WHITE) {
+                    send_msg(client_sock[me], "Checkmate! BLACK wins.\n");
+                    send_msg(client_sock[other], "Checkmate! BLACK wins.\n");
                 } else {
-                    fprintf(client_fp, "Checkmate! WHITE wins.\n");
+                    send_msg(client_sock[me], "Checkmate! WHITE wins.\n");
+                    send_msg(client_sock[other], "Checkmate! WHITE wins.\n");
                 }
-                fflush(client_fp);
                 game.turn = -1;
                 pthread_cond_signal(&turn_cond);
                 pthread_mutex_unlock(&game_mutex);
@@ -99,36 +106,38 @@ void *client_thread(void *arg) {
             }
         } else {
             if (!has_valid_moves(&game, me)) {
-                /* Stalemate */
                 broadcast_board();
-                fprintf(client_fp, "Stalemate! Game is a draw.\n");
-                fflush(client_fp);
+                send_msg(client_sock[me], "Stalemate! Game is a draw.\n");
+                send_msg(client_sock[other], "Stalemate! Game is a draw.\n");
                 game.turn = -1;
                 pthread_cond_signal(&turn_cond);
                 pthread_mutex_unlock(&game_mutex);
                 break;
             }
         }
+
         /* Prompt for move */
-        fprintf(client_fp, "Your move: ");
-        fflush(client_fp);
+        send_msg(client_sock[me], "Your move: ");
         pthread_mutex_unlock(&game_mutex);
 
         /* Read move from client */
-        if (fgets(buf, sizeof(buf), client_fp) == NULL) {
-            perror("Client disconnected");
+        ssize_t bytes_read = recv(client_sock[me], buf, sizeof(buf) - 1, 0);
+        if (bytes_read <= 0) {
+            perror("recv");
             exit(1);
         }
+        buf[bytes_read] = '\0';
+
         /* Remove newline */
         buf[strcspn(buf, "\r\n")] = '\0';
 
         pthread_mutex_lock(&game_mutex);
         int sr, sc, dr, dc;
         if (!parse_move(buf, &sr, &sc, &dr, &dc)) {
-            fprintf(client_fp, "Invalid input format. Use e2e4, etc.\n");
+            send_msg(client_sock[me], "Invalid input format. Use e2e4, etc.\n");
         } else {
             if (!make_move(&game, sr, sc, dr, dc)) {
-                fprintf(client_fp, "Invalid move. Try again.\n");
+                send_msg(client_sock[me], "Invalid move. Try again.\n");
             } else {
                 /* Move applied, switch turn */
                 broadcast_board();
@@ -136,7 +145,6 @@ void *client_thread(void *arg) {
                 pthread_cond_signal(&turn_cond);
             }
         }
-        fflush(client_fp);
         pthread_mutex_unlock(&game_mutex);
     }
 
@@ -154,7 +162,7 @@ int main() {
 
     /* Setup TCP socket */
     if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket");
+        perror("socket");
         exit(1);
     }
     memset(&serv_addr, 0, sizeof(serv_addr));
@@ -162,17 +170,17 @@ int main() {
     serv_addr.sin_port = htons(PORT);
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     if (bind(server_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Bind");
+        perror("bind");
         exit(1);
     }
     listen(server_sock, 2);
     printf("Waiting for two players to connect...\n");
 
     /* Accept two clients */
-    for(int i=0; i<2; i++) {
+    for (int i = 0; i < 2; i++) {
         client_sock[i] = accept(server_sock, NULL, NULL);
         if (client_sock[i] < 0) {
-            perror("Accept");
+            perror("accept");
             exit(1);
         }
         printf("Client %d connected.\n", i);
@@ -181,16 +189,16 @@ int main() {
     /* Launch client threads */
     pthread_t th[2];
     ThreadData td[2];
-    for(int i=0; i<2; i++) {
-        td[i].color = i;  /* White=0, Black=1 */
-        if(pthread_create(&th[i], NULL, client_thread, &td[i]) != 0) {
+    for (int i = 0; i < 2; i++) {
+        td[i].color = i;
+        if (pthread_create(&th[i], NULL, client_thread, &td[i]) != 0) {
             perror("pthread_create");
             exit(1);
         }
     }
 
     /* Wait for threads to finish */
-    for(int i=0; i<2; i++) {
+    for (int i = 0; i < 2; i++) {
         pthread_join(th[i], NULL);
     }
     printf("Game over. Server shutting down.\n");
