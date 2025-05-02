1bc7b9ef9d49507c5015a6cbb1eeca9f7c0e34b1
ahnahn <80297360+ahnahn@users.noreply.github.com>
2025-05-02 16:00:25 +0900

    Update client.c

--- client.c
+++ client.c
@@ -1,8 +1,10 @@
-/* client.c: Chess client using socket send/recv */
+/* client.c: Chess client using socket send/recv (with hostname resolution) */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <netdb.h>          // getaddrinfo
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
@@ -10,31 +12,47 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <server-ip> <port>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <server-hostname> <port>\n", argv[0]);
         exit(1);
     }
-    char *server_ip = argv[1];
-    int port = atoi(argv[2]);
+    char *server_host = argv[1];
+    char *port_str    = argv[2];
+
+    // 1) DNS 조회 / 주소 정보 얻기
+    struct addrinfo hints, *res, *rp;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family   = AF_UNSPEC;      // IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM;    // TCP
+
+    int rc = getaddrinfo(server_host, port_str, &hints, &res);
+    if (rc != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rc));
+        exit(1);
+    }
 
     int sockfd;
-    struct sockaddr_in servaddr;
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket");
-        exit(1);
-    }
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(port);
-    if (inet_pton(AF_INET, server_ip, &servaddr.sin_addr) <= 0) {
-        perror("inet_pton");
-        exit(1);
-    }
-    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
-        perror("connect");
-        exit(1);
-    }
-    printf("Connected to chess server %s:%d\n", server_ip, port);
+    // 2) 얻은 addrinfo 리스트를 순회하며 연결 시도
+    for (rp = res; rp != NULL; rp = rp->ai_next) {
+        sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (sockfd == -1) continue;
 
+        if (connect(sockfd, rp->ai_addr, rp->ai_addrlen) != -1) {
+            // 성공!
+            break;
+        }
+        close(sockfd);
+    }
+
+    if (rp == NULL) {
+        fprintf(stderr, "Could not connect to %s:%s\n", server_host, port_str);
+        freeaddrinfo(res);
+        exit(1);
+    }
+    freeaddrinfo(res);
+
+    printf("Connected to chess server %s:%s\n", server_host, port_str);
+
+    // 3) 게임 루프
     char buf[BUF_SIZE];
     while (1) {
         ssize_t n = recv(sockfd, buf, sizeof(buf) - 1, 0);
@@ -45,17 +63,17 @@ int main(int argc, char *argv[]) {
         buf[n] = '\0';
         printf("%s", buf);
 
-        /* If server prompts "Your move:", get user input */
+        // 서버가 "Your move:"를 보냈으면 사용자 입력 받아 전송
         if (strstr(buf, "Your move:") != NULL) {
-            char move[10];
-            if (fgets(move, sizeof(move), stdin) == NULL) {
+            char move[16];
+            if (!fgets(move, sizeof(move), stdin)) {
                 break;
             }
-            /* Remove newline */
             move[strcspn(move, "\r\n")] = '\0';
             send(sockfd, move, strlen(move), 0);
         }
     }
+
     close(sockfd);
     return 0;
 }
